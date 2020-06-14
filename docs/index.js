870dadaa865fa9b6b763acce560f35530c69a3f3
Marynia <marynia.kolak@gmail.com>
2020-06-13 23:22:08 -0500

    fixed files

--- docs/index.js
+++ docs/index.js
@@ -17,20 +17,6 @@ const {
  * CONFIG
 */
 
-
-$(document).on('click', '.panel-heading span.clickable', function(e){
-    var $this = $(this);
-  if(!$this.hasClass('panel-collapsed')) {
-    $this.parents('.panel').find('.panel-body').slideUp();
-    $this.addClass('panel-collapsed');
-    $this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
-  } else {
-    $this.parents('.panel').find('.panel-body').slideDown();
-    $this.removeClass('panel-collapsed');
-    $this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
-  }
-})
-
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoibGl4dW45MTAiLCJhIjoiY2locXMxcWFqMDAwenQ0bTFhaTZmbnRwaiJ9.VRNeNnyb96Eo-CorkJmIqg';
 
 const COLOR_SCALE = [
