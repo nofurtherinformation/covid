8b1e26f9ac984f577b1144e4f075338daddddfad
Dylan <d_h@mit.edu>
2021-01-24 15:24:07 -0600

    Redux Persist and Data Update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore} from 'redux';
-import reducer from './reducers';
 import { Provider } from 'react-redux';
 import './index.css';
 import WebFont from 'webfontloader';
 import registerServiceWorker from "./registerServiceWorker";
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { PersistGate } from 'redux-persist/integration/react';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2 ,
+  whitelist: ['dataParams', 'mapParams', 'currentData'] // only navigation will be persisted
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 const store = createStore(
-  reducer
-  // ,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  persistedReducer
+  ,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+const persistor = persistStore(store)
 
 
 WebFont.load({
@@ -21,11 +34,13 @@ WebFont.load({
 });
 
 ReactDOM.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
-      <App />
-    </Provider>,
-  // </React.StrictMode>,
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
